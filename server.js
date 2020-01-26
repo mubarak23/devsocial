e14f53a677529b9d204236a16f12ead1f2c2b27c
mubarak23 <mubarakaminu340@gmail.com>
2020-01-26 12:22:39 +0100

    setup our route

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const connectDB = require('./config/db');
 const app = express();
 
 //connect to db
-connectDB()
+connectDB();
 
+//Test api endpoint
 app.get('/', (req, res) => {
   res.send('API is running');
 });
+
+//Our Defined Route
+app.use('/api/user', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
