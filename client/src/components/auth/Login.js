c6afcc71f84d38326549107f31fd5dc34b87f6de
mubarak23 <mubarakaminu340@gmail.com>
2020-02-04 13:28:41 +0100

    connect login form with store

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,56 @@
-import React from 'react';
-
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+  const { email, password } = formData;
+
+  const onChange = event => {
+    setFormData({ ...formData, [event.target.name]: event.target.name });
+  };
+  const onSubmit = async event => {
+    event.preventDefault();
+    console.log('success');
+  };
+
+  return (
+    <div>
+      <Fragment>
+        <h1 className='large text-primary'>Sign In</h1>
+        <p className='lead'>
+          <i className='fas fa-user'>Sign into Your Account</i>
+        </p>
+        <form className='form' onSubmit={event => onSubmit(event)}>
+          <div className='form-group'>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={email}
+              onChange={event => onChange(event)}
+              required
+            />
+          </div>
+          <div>
+            <input
+              type='password'
+              placeholder='Account Password'
+              name='password'
+              value={password}
+              onChange={event => onChange(event)}
+              required
+            />
+          </div>
+          <input type='submit' className='btn btn-primary' value='Login' />
+        </form>
+        <p className='my-1'>
+          Don't Have an Account <Link to='/register'>Sign up</Link>
+        </p>
+      </Fragment>
+    </div>
+  );
 };
 
 export default Login;
