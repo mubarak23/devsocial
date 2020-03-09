a122a3279af88d68e7c8c7111c77b9ad02026170
mubarak23 <mubarakaminu340@gmail.com>
2020-03-09 12:48:26 +0100

    post with iterative postItem

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -15,4 +15,4 @@ export const ACCOUNT_DELETE = 'ACCOUNT_DELETE';
 export const GET_PROFILES = 'GET_PROFILES';
 export const GET_REPOS = 'GET_REPOS';
 export const GET_POSTS = 'GET_POSTS';
-export const POSTS_ERROR = 'POST_ERROR';
+export const POST_ERROR = 'POST_ERROR';
