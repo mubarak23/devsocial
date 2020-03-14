a3fa3b767773bec4b112589a0e8ee19d19444479
mubarak23 <mubarakaminu340@gmail.com>
2020-03-14 17:12:23 +0100

    preparation for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var cors = require('cors');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,9 +10,6 @@ const app = express();
 connectDB();
 
 //Test api endpoint
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
 //middleware
 //setup the middlware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,6 +30,16 @@ app.use('/api/user', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profiles', require('./routes/api/profile'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
