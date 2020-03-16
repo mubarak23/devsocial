721951662b5c0c2a21231263e67e99804db14661
mubarak23 <mubarakaminu340@gmail.com>
2020-03-16 02:16:58 +0100

    working on correct mlab error

--- client/src/components/routing/routes.js
+++ client/src/components/routing/routes.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Register from './components/auth/Register';
+import Register from '../auth/Register';
 import Login from '../auth/Login';
-import Alert from '../components/layout/Alert';
+import Alert from '../layout/Alert';
 import { Provider } from 'react-redux';
-import createProfile from '../components/profile-forms/createProfile';
-import editProfile from '../components/profile-forms/EditProfile';
-import AddExperience from '../components/profile-forms/AddExperience';
-import AddEducation from '../components/profile-forms/AddEducation';
-import Profiles from '../components/profiles/Profiles';
-import Profile from '../components/profiles/Profile';
-import Posts from '../components/posts/Posts';
-import Post from '../components/posts/Post';
-import Dashboard from '../components/dashboard/Dashboard';
-import PrivateRoute from '../components/routing/PrivateRoute';
+import createProfile from '../profile-forms/createProfile';
+import editProfile from '../profile-forms/EditProfile';
+import AddExperience from '../profile-forms/AddExperience';
+import AddEducation from '../profile-forms/AddEducation';
+import Profiles from '../profiles/Profiles';
+import Profile from '../profiles/Profile';
+import Posts from '../posts/Posts';
+import Post from '../posts/Post';
+import Dashboard from '../dashboard/Dashboard';
+import PrivateRoute from '../routing/PrivateRoute';
 
 const Routes = () => {
   return (
