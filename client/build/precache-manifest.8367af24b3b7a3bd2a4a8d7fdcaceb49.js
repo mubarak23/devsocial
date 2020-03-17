c2433487236d74da63453b255878dce86587603c
mubarak23 <mubarakaminu340@gmail.com>
2020-03-17 13:41:32 +0100

    build done

---
+++ client/build/precache-manifest.8367af24b3b7a3bd2a4a8d7fdcaceb49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54a378105af3b04e8542659f6863635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbed3c3f66bc4fe089e0",
+    "url": "/static/css/main.d8116c63.chunk.css"
+  },
+  {
+    "revision": "58686ba4609df577dddd",
+    "url": "/static/js/2.d83def44.chunk.js"
+  },
+  {
+    "revision": "dbed3c3f66bc4fe089e0",
+    "url": "/static/js/main.9e87a5bb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
